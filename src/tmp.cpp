21c0a1a113ce98c12e9752ae8e34e1c30ccdacb0
Mokbel Karam <mkaram@MacBook-Pro.local>
2022-04-09 20:49:11 -0600

    Changed the name of the project

--- src/tmp.cpp
+++ src/tmp.cpp
@@ -1,3 +1,3 @@
-#include "project/tmp.hpp"
+#include "execgraph/tmp.hpp"
 
 int tmp::add(int a, int b) { return a + b; }
--- test/src/tmp_test.cpp
+++ test/src/tmp_test.cpp
@@ -1,4 +1,4 @@
-#include "project/tmp.hpp"
+#include "execgraph/tmp.hpp"
 
 #include <gtest/gtest.h>
 
